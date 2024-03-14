ad48f0cb0b4305cbc92571baef1627103c5c13f0
yuta925 <yutarou041@gmail.com>
2024-03-14 23:22:57 +0900

    初級編終わりました

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,11 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
+  const checkbox = document.getElementById("check");
+  const text = document.getElementById("text");
 
+  if (checkbox.checked) {
+    text.style.backgroundColor = "#ff0000"
+  } else {
+    text.style.backgroundColor = "#000000"
+  }
 }
---
+++ src/scripts/station15.js
@@ -0,0 +1,8 @@
+function displayList() {
+    const fruits = document.getElementById("fruits");
+    fruits.innerHTML = `<ul>
+                          <li>リンゴ</li>
+                          <li>イチゴ</li>
+                          <li>ブドウ</li>
+                        </ul>`;
+}
